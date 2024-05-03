8df9b87dd3e2c845e1b6547eaecb97af46f9b857
MaxLiebsch <liebsch@dipmaxexport.com>
2024-05-03 07:15:23 +0200

    fix: logout

--- src/server/actions/logout.ts
+++ src/server/actions/logout.ts
@@ -1,10 +1,10 @@
 "use client";
 
+import { createWebClient } from "@/web/appwrite";
 import { redirect } from "next/navigation";
-import { createSessionClient } from "../appwrite";
 
 export async function logoutAction() {
-  const { account } = await createSessionClient();
+  const{ account} = await createWebClient() 
   await account.deleteSession("current");
 
   redirect("/auth/signin");
