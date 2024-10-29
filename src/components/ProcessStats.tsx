58e584d60699e91353d8f435d6c1e6042f18340f
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-29 16:47:26 +0100

    fix: distinct total products

--- src/components/ProcessStats.tsx
+++ src/components/ProcessStats.tsx
@@ -3,11 +3,10 @@ import { IProcessStats } from "@/types/ProcessStats";
 import React from "react";
 
 const ProcessStats = ({ processStats }: { processStats: IProcessStats }) => {
-  console.log("processStats:", processStats);
   return (
     <div>
       <h3 className="text-base font-semibold leading-6 text-gray-900 flex flex-row space-x-1 items-center">
-        ProcessStats
+        ProcessStats ({processStats.distinctProducts})
       </h3>
       <div className="grid grid-cols-3">
         <ul className="col-span-1">
